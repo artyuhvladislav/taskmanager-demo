3db3b900014eb33970cae2db9de4e690d64ba153
artyuhvladislav <workto22@gmail.com>
2023-10-04 19:21:39 +0200

    add taskList class component

--- src/components/tasksList/tasksList.js
+++ src/components/tasksList/tasksList.js
@@ -1,3 +1,31 @@
-export const createTasksListTemplate = () => `
-      <div class="board__tasks"></div>
-  `;
+import { createElement } from '../../utils';
+
+const createTaskListTemplate = () => {
+    return (
+        `<div class="board__tasks">
+        </div>
+        `
+    );
+};
+
+export class TasksList {
+
+    constructor() {
+        this._element = null;
+    }
+
+    getTemplate() {
+        return createTaskListTemplate();
+    }
+
+    getElement() {
+        if (!this._element) {
+            this._element = createElement(this.getTemplate());
+        }
+        return this._element;
+    }
+
+    removeElement() {
+        this._element = null;
+    }
+}
