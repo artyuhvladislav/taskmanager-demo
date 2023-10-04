8fe8a34fd2b0f76e09d2aec99aff51508a51ac0a
artyuhvladislav <workto22@gmail.com>
2023-10-04 19:19:32 +0200

    add sort class component

--- src/components/sort/sort.js
+++ src/components/sort/sort.js
@@ -1,7 +1,31 @@
-export const createSortTemplate = () => `
+import { createElement } from '../../utils';
+
+const createSortTemplate = () => `
   <div class="board__sort-list">
     <a href="#" class="board__sort-item">SORT BY DEFAULT</a>
     <a href="#" class="board__sort-item">SORT BY DATE up</a>
     <a href="#" class="board__sort-item">SORT BY DATE down</a>
   </div>
   `;
+
+export class Sort {
+
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createSortTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
