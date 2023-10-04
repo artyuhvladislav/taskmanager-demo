d847544b5ceef44928fbb1347d87959e64646ff5
artyuhvladislav <workto22@gmail.com>
2023-10-04 19:18:56 +0200

    add loadMoreButton class component

--- src/components/loadButton/loadButton.js
+++ src/components/loadButton/loadButton.js
@@ -1,3 +1,26 @@
-export const createLoadButtonTemplate = () => `
+import { createElement } from '../../utils';
+
+const createLoadButtonTemplate = () => `
    <button class="load-more" type="button">load more</button>
   `;
+export class LoadButton {
+
+   constructor() {
+      this._element = null;
+   }
+
+   getTemplate() {
+      return createLoadButtonTemplate();
+   }
+
+   getElement() {
+      if (!this._element) {
+         this._element = createElement(this.getTemplate());
+      }
+      return this._element;
+   }
+
+   removeElement() {
+      this._element = null;
+   }
+}
